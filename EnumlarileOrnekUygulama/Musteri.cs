f2eca246ba969fb12a2922e1640a8553d0fe9dd0
Pinaraltinkaya45 <189837954+Pinaraltinkaya45@users.noreply.github.com>
2024-12-05 20:04:07 +0300

    Add project files.

---
+++ EnumlarileOrnekUygulama/Musteri.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumlarileOrnekUygulama
+{
+    public class Musteri
+    {
+        public int id {  get; set; }
+        public string isim { get; set; }
+        public string soyisim { get; set; }
+        public string cinsiyet { get; set; }
+        public string emailAdres { get; set; }
+
+        public Musteri(int id, string isim, string soyisim, string cinsiyet, string emailAdres)
+        {
+            this.id = id;
+            this.isim = isim;
+            this.soyisim = soyisim;
+            this.cinsiyet = cinsiyet;
+            this.emailAdres = emailAdres;
+        }
+
+        public static ArrayList  musteriler = new ArrayList();
+
+
+        public MusteriDurum musteriEkle(Musteri M1)
+        {
+            musteriler.Add(M1); // musteriyi koleksiyona ekledik
+
+            return MusteriDurum.kayitBasarili;
+
+           
+        }
+
+    }
+}
---
+++ EnumlarileOrnekUygulama/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumlarileOrnekUygulama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri(1,"Enes","Bayram","Erkek","enes.bayram@gmail.com");
+
+           MusteriDurum donenDurum = musteri.musteriEkle(musteri);
+
+            if(donenDurum == MusteriDurum.kayitBasarili)
+            {
+                Console.WriteLine("Müşteri başarılı bir şekilde eklenmiştir.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
